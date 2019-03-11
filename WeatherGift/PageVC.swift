8ddcfde403a75a6164d5ae8eda3d2291b1a2d020
Anna Guo <annaguo03@gmail.com>
2019-03-11 13:19:45 -0400

    7.6 Video Commit: -To TableView and Back with Segues

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -63,13 +63,29 @@ class PageVC: UIPageViewController {
             listButton.setBackgroundImage(UIImage(named: "listbutton"),for: .normal)
             listButton.setBackgroundImage(UIImage(named: "listbutton-highlighted"),for: .highlighted)
             listButton.addTarget(self, action:
-                #selector(segueToLocationsListVC), for:
+                #selector(segueToListVC), for:
                 .touchUpInside)
             view.addSubview(listButton)
     }
+    //MARK:- Segues
+    @objc func segueToListVC(){
+        performSegue(withIdentifier: "ToListVC", sender: nil)
+    }
     
-    @objc func segueToLocationsListVC(){
-        print("Hey you clicked me!")
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToListVC" {
+            let destination = segue.destination as! ListVC
+            destination.locationsArray = locationsArray
+            destination.currentPage = currentPage
+            
+        }
+        
+    }
+    
+    @IBAction func unwindFromListVC(sender: UIStoryboardSegue){
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        setViewControllers([createDetailVC(forPage: currentPage)], direction: .forward, animated: false, completion: nil)
     }
     
     
